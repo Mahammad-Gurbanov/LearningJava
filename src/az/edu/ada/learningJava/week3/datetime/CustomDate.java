5362318022ab34b7bfb341ef9d9943a1257ac10c
MahammadGurbanov <mehemmedbusiness@icloud.com>
2026-02-22 15:01:25 +0400

    Implement displayFormatted method

--- src/az/edu/ada/learningJava/week3/datetime/CustomDate.java
+++ src/az/edu/ada/learningJava/week3/datetime/CustomDate.java
@@ -77,7 +77,7 @@ public class CustomDate {
     }
 
     public void displayDate(){
-        System.out.printf("%d/%d/%d", this.day, this.month, this.year);
+        System.out.printf("%d/%d/%d\n", this.day, this.month, this.year);
     }
 
     /**
@@ -109,5 +109,26 @@ public class CustomDate {
         else if (date1.getDay() < date2.getDay()) return 1;
         else if (date1.getDay() > date2.getDay()) return -1;
         return 0;
+    }
+
+    public void displayFormatted(){
+        String month;
+        month = switch (this.month){
+            case 1 -> "Jan";
+            case 2 -> "Feb";
+            case 3 -> "Mar";
+            case 4 -> "Apr";
+            case 5 -> "May";
+            case 6 -> "Jun";
+            case 7 -> "July";
+            case 8 -> "Aug";
+            case 9 -> "Sep";
+            case 10 -> "Oct";
+            case 11 -> "Nov";
+            case 12 -> "Dec";
+            default -> "";
+        };
+        System.out.printf("%d %s %d\n", this.day, month, this.year);
+
     }
 }
--- src/az/edu/ada/learningJava/week3/datetime/Main.java
+++ src/az/edu/ada/learningJava/week3/datetime/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         // Test dislayDate method
         CustomDate date1 = new CustomDate(22, 3, 2025);
-        CustomDate date2 = new CustomDate(22, 3, 2025);
+        CustomDate date2 = new CustomDate(22, 9, 2015);
         System.out.println(CustomDate.compare(date1, date2));
+        date1.displayFormatted();
+        date2.displayFormatted();
     }
 }

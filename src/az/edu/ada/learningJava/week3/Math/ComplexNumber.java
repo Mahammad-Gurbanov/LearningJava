dd4523012e6ef95d3cdec31ec0e2b2bb6a2177a6
MahammadGurbanov <mehemmedbusiness@icloud.com>
2026-02-22 15:58:09 +0400

    Implement toString method

--- src/az/edu/ada/learningJava/week3/Math/ComplexNumber.java
+++ src/az/edu/ada/learningJava/week3/Math/ComplexNumber.java
@@ -22,4 +22,15 @@ public class ComplexNumber {
                 this.imag == otherComplexNum.getImag();
     }
 
+    public String toString(){
+        String res;
+        if (this.imag > 0) res = String.format("%.2f + %.2fi",
+                this.real, this.imag);
+        else if (this.imag == 0) res = Double.toString(this.real);
+        else res = String.format("%.2f - %.2fi",
+                    this.real, Math.abs(this.imag));
+
+        return res;
+    }
+
 }
--- src/az/edu/ada/learningJava/week3/datetime/Main.java
+++ src/az/edu/ada/learningJava/week3/datetime/Main.java
@@ -5,10 +5,13 @@ import az.edu.ada.learningJava.week3.Math.ComplexNumber;
 public class Main {
     public static void main(String[] args) {
         ComplexNumber num1 = new ComplexNumber(3, 4);
-        ComplexNumber num2 = new ComplexNumber(8, 10);
-        ComplexNumber num3 = new ComplexNumber(3, 4);
+        ComplexNumber num2 = new ComplexNumber(8, -10);
+        ComplexNumber num3 = new ComplexNumber(3, 0);
+        ComplexNumber num4 = new ComplexNumber(0, 0);
 
-        System.out.println(num1.equals(num2));
-        System.out.println(num1.equals(num3));
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
     }
 }

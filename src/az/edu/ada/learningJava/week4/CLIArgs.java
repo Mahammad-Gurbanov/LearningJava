c5dc102cbd7fab71622947406fbd8264a8870289
MahammadGurbanov <mehemmedbusiness@icloud.com>
2026-02-27 18:22:29 +0400

    Read arbitrary number of ints & print them

--- src/az/edu/ada/learningJava/week4/CLIArgs.java
+++ src/az/edu/ada/learningJava/week4/CLIArgs.java
@@ -1,5 +1,6 @@
 package az.edu.ada.learningJava.week4;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CLIArgs {
@@ -8,5 +9,13 @@ public class CLIArgs {
         System.out.print("User input: ");
         String user_inp = scanner.nextLine();
         System.out.println(user_inp);
+
+        System.out.println("Enter integers: ");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while (scanner.hasNextInt()){
+            int num = scanner.nextInt();
+            numbers.add(num);
+        }
+        System.out.println(numbers);
     }
 }

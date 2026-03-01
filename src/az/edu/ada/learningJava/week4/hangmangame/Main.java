26f91d38daf5d9842ebcda11676f388e601bf95a
MahammadGurbanov <mehemmedbusiness@icloud.com>
2026-03-01 22:58:53 +0400

    Implement the hangman game

--- src/az/edu/ada/learningJava/week4/hangmangame/Main.java
+++ src/az/edu/ada/learningJava/week4/hangmangame/Main.java
@@ -1,14 +1,40 @@
 package az.edu.ada.learningJava.week4.hangmangame;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Player p1 = new Player("P1");
-        HangmanGame game = new HangmanGame("ABC");
-        System.out.println(game.inWord('A'));
-        System.out.println(game.inWord('Z'));
-        game.displayState();
-        game.updateState('C');
-        game.displayState();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter a secret word: ");
+        String secretWord = sc.nextLine();
+        Player player = new Player(name);
+        HangmanGame game = new HangmanGame(secretWord.toLowerCase());
 
+        while (true){
+            System.out.println("You have " + player.getAvailableGuesses() +
+                    " guesses");
+            game.displayState();
+            System.out.print("write your guess: ");
+            char guess = sc.next().toLowerCase().charAt(0);
+            player.addGuess(guess);
+            if (game.inWord(guess)){
+                game.updateState(guess);
+                if (game.hasFound()) {
+                    System.out.println("Congratulations!");
+                    System.out.println("The word is: " + game.getSecretWord());
+                    break;
+                }
+            } else {
+                player.decrementAvailableGuesses();
+                if (!player.hasGuesses()){
+                    System.out.println("You lost");
+                    System.out.println("The word was: " + game.getSecretWord());
+                    break;
+                }
+            }
+            System.out.println(player.getGuesses());
+        }
     }
 }

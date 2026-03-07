886c06ca8ce37c2ea82a7bd131f6fabfdf68fed6
MahammadGurbanov <mehemmedbusiness@icloud.com>
2026-03-07 19:33:30 +0400

    Implement Square that extends Rectangle

---
+++ src/az/edu/ada/learningJava/week5/Square.java
@@ -0,0 +1,9 @@
+package az.edu.ada.learningJava.week5;
+
+public class Square extends Rectangle{
+    int side;
+    public Square(int side){
+        super(side, side);
+        this.side = side;
+    }
+}

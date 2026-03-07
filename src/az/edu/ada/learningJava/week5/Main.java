007088059b5f5dfed1e562d6a16f33c456a673f5
MahammadGurbanov <mehemmedbusiness@icloud.com>
2026-03-07 18:48:43 +0400

    Test Customer class

---
+++ src/az/edu/ada/learningJava/week5/Main.java
@@ -0,0 +1,9 @@
+package az.edu.ada.learningJava.week5;
+
+public class Main {
+    public static void main(String[] args) {
+        Customer c1 = new Customer(123, "ABC", 'm');
+        System.out.println(c1);
+        Customer c2 = new Customer(111, "As", 'q');
+    }
+}

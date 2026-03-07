40671c050a0e8c87ca68c4ee249ef08b42d0efbd
MahammadGurbanov <mehemmedbusiness@icloud.com>
2026-03-07 19:06:58 +0400

    Test unique id at initilization

--- src/az/edu/ada/learningJava/week5/Main.java
+++ src/az/edu/ada/learningJava/week5/Main.java
@@ -3,7 +3,8 @@ package az.edu.ada.learningJava.week5;
 public class Main {
     public static void main(String[] args) {
         Customer c1 = new Customer(123, "ABC", 'm');
-        System.out.println(c1);
-        Customer c2 = new Customer(111, "As", 'q');
+        Account a1 = new Account(1, c1, 123.4);
+        Account a2 = new Account(1, c1);
+
     }
 }

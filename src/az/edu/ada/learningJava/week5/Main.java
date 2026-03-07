3536f0615ad370d66ced3d0d4fe7b96f60bdb340
MahammadGurbanov <mehemmedbusiness@icloud.com>
2026-03-07 19:17:09 +0400

    Test deposit & withdraw methods

--- src/az/edu/ada/learningJava/week5/Main.java
+++ src/az/edu/ada/learningJava/week5/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Customer c1 = new Customer(123, "ABC", 'm');
         Account a1 = new Account(1, c1, 123.4);
-        Account a2 = new Account(1, c1);
-
+        a1.deposit(100);
+        a1.withdraw(12);
+        System.out.println(a1.getBalance());
     }
 }

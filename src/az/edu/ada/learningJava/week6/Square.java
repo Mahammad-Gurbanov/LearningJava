2d319391a0f0342a5bc8f80a02a811c2e656dabd
MahammadGurbanov <mehemmedbusiness@icloud.com>
2026-03-10 16:38:19 +0400

    Create Square class

---
+++ src/az/edu/ada/learningJava/week6/Square.java
@@ -0,0 +1,27 @@
+package az.edu.ada.learningJava.week6;
+
+public class Square extends Rectangle{
+    public Square(){};
+
+    public Square(double side){
+        super(side, side);
+    }
+
+    public Square(double side, String color, boolean filled){
+        super(side, side, color, filled);
+    }
+
+    public double getSide(){
+        return super.getLength();
+    }
+
+    public void setSide(double side){
+        super.setLength(side);
+        super.setWidth(side);
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Square[%s]", super.toString());
+    }
+}

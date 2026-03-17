d723cfb45bb8d527f55d954a9b5ca8a48bc2fcba
MahammadGurbanov <mehemmedbusiness@icloud.com>
2026-03-17 19:27:51 +0400

    Create CustomStack with constructors

--- src/az/edu/ada/learningJava/week7/CustomStack.java
+++ src/az/edu/ada/learningJava/week7/CustomStack.java
@@ -1,4 +1,16 @@
 package az.edu.ada.learningJava.week7;
 
 public class CustomStack {
+    private int size = 20;
+    private int[] stack;
+
+    public CustomStack(){
+        stack = new int[size];
+    }
+
+    public CustomStack(int size){
+        if (size <= 0) throw new
+                InvalidSizeException("Size must be a positive Integer");
+        this.size = size;
+    }
 }

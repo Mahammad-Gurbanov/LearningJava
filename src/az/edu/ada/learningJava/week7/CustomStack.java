8399e51ff3bbfa77a388d6ba2aad1fa415a41724
MahammadGurbanov <mehemmedbusiness@icloud.com>
2026-03-17 19:47:56 +0400

    Implement shrinking & expanding

--- src/az/edu/ada/learningJava/week7/CustomStack.java
+++ src/az/edu/ada/learningJava/week7/CustomStack.java
@@ -1,8 +1,37 @@
 package az.edu.ada.learningJava.week7;
 
 public class CustomStack {
-    private int size = 20;
+    private int size = 5;
     private int[] stack;
+    private int elementCount = 0;
+
+    private boolean shouldExpand(){
+        return elementCount * 2 >= size;
+    }
+
+    private boolean shouldShrink(){
+        return elementCount * 4 < size && elementCount > 20;
+    }
+
+    private void expandStack(){
+        int[] oldStack = stack;
+        size *= 2;
+        stack = new int[size];
+        for (int i = 0; i < oldStack.length; i++){
+            stack[i] = oldStack[i];
+        }
+
+    }
+
+    private void shrinkStack(){
+        int[] oldStack = stack;
+        size /= 2;
+        stack = new int[size];
+        for (int i = 0; i < elementCount; i++){
+            stack[i] = oldStack[i];
+        }
+
+    }
 
     public CustomStack(){
         stack = new int[size];

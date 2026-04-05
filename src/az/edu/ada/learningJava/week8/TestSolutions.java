8a45c97ba98a74059976ab63ca8e836de602e47a
MahammadGurbanov <mehemmedbusiness@icloud.com>
2026-04-05 13:42:34 +0400

    Create and print the list of accounts

--- src/az/edu/ada/learningJava/week8/TestSolutions.java
+++ src/az/edu/ada/learningJava/week8/TestSolutions.java
@@ -1,20 +1,23 @@
 package az.edu.ada.learningJava.week8;
+import java.util.ArrayList;
 
 public class TestSolutions {
     public static void main(String[] args) {
-        String[] exampleNames = {"John", "Fred", "Bruce", "Fred", "Robert"};
-        DistinctWords words = new DistinctWords(exampleNames);
-        words.removeDuplicates();
-        words.sortAlphabetically();
-        for (String name : words.getNames()){
-            System.out.println(name);
-        }
+        Account account1 = new Account("J", "M", 123.45);
+        Account account2 = new Account("A", "Z", 999);
+        Account account3 = new Account("B", "C", 12.45);
+        Account account4 = new Account("ABC", "XYZ", 123.45);
+        Account account5 = new Account("ABC", "XYZ", 9999);
 
-        Store myStore = new Store(20);
-        int[] breadNeeded = {2, 4, 6, 3, 8};
-        int totalCount = myStore.countPeopleBuyingBread(breadNeeded);
-        System.out.println(totalCount);
-        System.out.println(myStore.getTotalBread());
+        ArrayList<Account> accounts = new ArrayList<>();
+        accounts.add(account1);
+        accounts.add(account2);
+        accounts.add(account3);
+        accounts.add(account4);
+        accounts.add(account5);
+        for (Account account : accounts){
+            System.out.println(account);
+        }
 
     }
 

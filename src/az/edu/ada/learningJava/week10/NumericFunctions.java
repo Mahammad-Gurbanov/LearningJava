19eaa552b3f7ed55ed6db730c21508ca987ccfbb
MahammadGurbanov <mahammadgurbanovbusiness@gmail.com>
2026-05-09 21:19:17 +0400

    add constructor

---
+++ src/az/edu/ada/learningJava/week10/NumericFunctions.java
@@ -0,0 +1,10 @@
+package az.edu.ada.learningJava.week10;
+
+public class NumericFunctions <N extends Number> {
+    N number;
+
+    public NumericFunctions(N number){
+        this.number = number;
+    }
+
+}

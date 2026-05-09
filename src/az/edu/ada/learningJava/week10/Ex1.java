ea1279fcdb1fa79be0f97be5d032ad95ba308dea
MahammadGurbanov <mahammadgurbanovbusiness@gmail.com>
2026-05-09 20:43:36 +0400

    add generic retrieval of middle element

--- src/az/edu/ada/learningJava/week10/Ex1.java
+++ src/az/edu/ada/learningJava/week10/Ex1.java
@@ -4,11 +4,13 @@ public class Ex1 {
     public static void main(String[] args) {
         Integer[] arr1 = {1, 2, 3};
         Double[] arr2 = {0.1, 0.2, 0.3};
-        String[] arr3 = {"str1", "str2", "str3"};
+        String[] arr3 = {"str1", "str2", "str3", "str4"};
+        Character[] arr4 = new Character[4];
 
-        printArray(arr1, 1, 3);
-        printArray(arr2, 0, 2);
-        printArray(arr3, 2, 3);
+        System.out.println(getMiddleItem(arr1));
+        System.out.println(getMiddleItem(arr2));
+        System.out.println(getMiddleItem(arr3));
+        System.out.println(getMiddleItem(arr4));
     }
 
     public static <T> void printArray(T[] arr){
@@ -23,6 +25,15 @@ public class Ex1 {
         }
         for (int i = lowerIdx; i < upperIdx; i++){
             System.out.println(arr[i]);
+        }
+    }
+
+    public static <T> T getMiddleItem(T[] arr){
+        if (arr.length == 0) throw new IllegalArgumentException("Array can't be empty");
+        if (arr.length % 2 == 0){
+            return arr[arr.length / 2 - 1];
+        } else {
+            return arr[arr.length / 2];
         }
     }
 }

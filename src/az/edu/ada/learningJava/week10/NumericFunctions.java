952f87e5fc2a344f9095b6358878d8478ac243ce
MahammadGurbanov <mahammadgurbanovbusiness@gmail.com>
2026-05-09 21:37:04 +0400

    add getReciprocal

--- src/az/edu/ada/learningJava/week10/NumericFunctions.java
+++ src/az/edu/ada/learningJava/week10/NumericFunctions.java
@@ -7,4 +7,20 @@ public class NumericFunctions <N extends Number> {
         this.number = number;
     }
 
+    public static void main(String[] args) {
+        NumericFunctions<Integer> intFunction = new NumericFunctions<>(3);
+        System.out.println(intFunction.getReciprocal());
+
+        NumericFunctions<Double> doubleFunction = new NumericFunctions<>(3.9);
+        System.out.println(doubleFunction.getReciprocal());
+
+        NumericFunctions<Float> floatFunction = new NumericFunctions<>(3.2F);
+        System.out.println(floatFunction.getReciprocal());
+    }
+
+    public double getReciprocal(){
+        double reciprocal = 1 / number.doubleValue() * 100;
+        reciprocal = Math.round(reciprocal);
+        return reciprocal / 100;
+    }
 }

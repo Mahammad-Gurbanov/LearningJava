d45a755ea7c7fb67c6e30198fe0583c4a2276fde
MahammadGurbanov <mahammadgurbanovbusiness@gmail.com>
2026-05-09 21:20:10 +0400

    mend

--- src/az/edu/ada/learningJava/week10/NumericFunctions.java
+++ src/az/edu/ada/learningJava/week10/NumericFunctions.java
@@ -1,7 +1,7 @@
 package az.edu.ada.learningJava.week10;
 
 public class NumericFunctions <N extends Number> {
-    N number;
+    private N number;
 
     public NumericFunctions(N number){
         this.number = number;

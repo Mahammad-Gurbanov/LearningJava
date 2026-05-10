50b0181c475426b9da6aa3f41c66f4e6622e0314
MahammadGurbanov <mahammadgurbanovbusiness@gmail.com>
2026-05-10 23:14:21 +0400

    add Transaction

---
+++ src/az/edu/ada/learningJava/week11/Transaction.java
@@ -0,0 +1,20 @@
+package az.edu.ada.learningJava.week11;
+
+public class Transaction {
+    private String category;
+    private double amount;
+
+    public Transaction(String category, double amount){
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+}

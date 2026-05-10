6253c80fe22e78ea649846a099dd6abec008d2a9
MahammadGurbanov <mahammadgurbanovbusiness@gmail.com>
2026-05-10 23:27:39 +0400

    calculate total tech transaction amount

--- src/az/edu/ada/learningJava/week11/Transaction.java
+++ src/az/edu/ada/learningJava/week11/Transaction.java
@@ -1,5 +1,8 @@
 package az.edu.ada.learningJava.week11;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transaction {
     private String category;
     private double amount;
@@ -15,6 +18,23 @@ public class Transaction {
 
     public double getAmount(){
         return amount;
+    }
+
+    public static void main(String[] args) {
+        List<Transaction> transactions = new ArrayList<>(List.of(
+                new Transaction("Tech", 1000),
+                new Transaction("Tech", 2700),
+                new Transaction("Grocery", 23.64),
+                new Transaction("Grocery", 17.99),
+                new Transaction("Rent", 4000),
+                new Transaction("Tech", 888)
+        ));
+
+        double totalTechTransactionAmount = transactions.stream()
+                .filter(t -> t.getCategory().equals("Tech"))
+                .mapToDouble(Transaction::getAmount)
+                .sum();
+        System.out.println(totalTechTransactionAmount);
     }
 
 }
